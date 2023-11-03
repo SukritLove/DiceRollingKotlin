9b4dcfa21d87914107b8c3bfd50427635da045c8
Sukrit Rerk-u-cote <sukrit_love@Sukrits-MacBook-Pro.local>
2023-11-03 10:12:36 +0700

    Dice Rolling Proj. Kotlin [Using Image Dice]

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroll
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,24 +12,60 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollBtn: Button = findViewById(R.id.rollDice)
-        val resultText: TextView = findViewById(R.id.diceNumber1)
-        val resultText2: TextView = findViewById(R.id.diceNumber2)
+        val diceBlock1: ImageView = findViewById(R.id.dice1)
+
+        val diceBlock2: ImageView = findViewById(R.id.dice2)
 
         rollBtn.setOnClickListener {
             val toast = Toast.makeText(this, "Rolling the Dice!!!", Toast.LENGTH_SHORT)
             toast.show()
 
-            rollDice(resultText)
-            rollDice(resultText2)
+            rollDice(diceBlock1)
+            rollDice(diceBlock2)
+
         }
 
 
     }
 
-    private fun rollDice(result : TextView) {
+
+    private fun rollDice(result: ImageView) {
         val dice = Dice(6)
+
         val diceRoll = dice.roll()
-        result.text = diceRoll.toString()
+        val drResult = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        result.setImageResource(drResult)
+        result.contentDescription = diceRoll.toString()
+
+        // Condition 1 : Separate variable with Logic
+        /*val diceRoll = dice.roll()
+        when(diceRoll){
+            1 -> result.setImageResource(R.drawable.dice_1)
+            2 -> result.setImageResource(R.drawable.dice_2)
+            3 -> result.setImageResource(R.drawable.dice_3)
+            4 -> result.setImageResource(R.drawable.dice_4)
+            5 -> result.setImageResource(R.drawable.dice_5)
+            6 -> result.setImageResource(R.drawable.dice_6)
+        }*/
+
+        // Condition 2 : Inline
+        /*when (dice.roll()) {
+            1 -> result.setImageResource(R.drawable.dice_1)
+            2 -> result.setImageResource(R.drawable.dice_2)
+            3 -> result.setImageResource(R.drawable.dice_3)
+            4 -> result.setImageResource(R.drawable.dice_4)
+            5 -> result.setImageResource(R.drawable.dice_5)
+            6 -> result.setImageResource(R.drawable.dice_6)
+        }*/
+
+
     }
 
     class Dice(private var numSides: Int) {
@@ -37,4 +73,5 @@ class MainActivity : AppCompatActivity() {
             return (1..numSides).random()
         }
     }
+
 }
